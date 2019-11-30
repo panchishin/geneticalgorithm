e05ae81e75c145a3e2d9a4696557a634296ff9b2
Anton Panchishin <anton.panchishin@gmail.com>
2019-11-29 20:06:41 -0500

    improve ui

--- examples/islandHopBrowser.js
+++ examples/islandHopBrowser.js
@@ -4,17 +4,23 @@ const context = canvas.getContext('2d');
 
 MAX_SIZE = 600;
 
-let islands = [[0.5,0.5],[2.5,0.5],[4.5,0.5],[6.5,0.5],[8.5,0.5],
-[0.5,2.5],[0.5,4.5],[0.5,6.5],[0.5,8.5],[8.5,8.5]];
+let islands = [[0.5,0.5],                                // upper left
+               [2.5,0.5],[4.5,0.5],[6.5,0.5],[8.5,0.5],  // x column 
+               [0.5,2.5],[0.5,4.5],[0.5,6.5],[0.5,8.5],  // y column
+               [8.5,8.5]];                               // sweet spot
 
 
+Math.sqr = function(x) { return Math.pow(x,2) }
+function cloneJSON( item ) { return JSON.parse ( JSON.stringify ( item ) ) }
 
 function positionScore(x,y){
-    return islands.map(function distance(island) {
-        return (island[0]/2.+island[1]) * ( 1 - Math.sqrt(Math.square(x - island[0]) + Math.square(y - island[1])/2) )
+    return islands.map(function(island) {
+        let islandValue = island[0]/2.+island[1]
+        let distance = Math.sqrt( (Math.sqr(x - island[0]) + Math.sqr(y - island[1])) / 2 )
+        if (distance > .4) { return -10 }
+        return (distance + 1) * islandValue
     }).reduce(function(a,b) { return Math.max(a,b) })
 }
-
 
 function drawCircle(x,y,s,color) {
     context.fillStyle = "hsla("+color+",90%,40%,1)";
@@ -25,7 +31,6 @@ function drawCircle(x,y,s,color) {
     context.stroke();
 }
 
-
 function drawIsland(x,y) {
     drawCircle(x,y,.4,90)
     context.fillStyle = "hsla(0,0%,0%,1)";
@@ -34,11 +39,10 @@ function drawIsland(x,y) {
     context.fillText(Math.round(10*positionScore(x,y)),x*MAX_SIZE/10., y*MAX_SIZE/10.);
 }
 
-
-function drawFrog(x,y) {
-    drawCircle(x,y,.1,270)
+function drawFrog(x,y) { 
+    let score = positionScore(x,y)
+    drawCircle(x,y,.1,score > 0 ? 270: 320) 
 }
-
 
 function draw() {
     // clear the screen
@@ -56,12 +60,11 @@ function draw() {
     window.requestAnimationFrame(draw);
 }
 
-window.onload = function() {
-    window.requestAnimationFrame(draw);
-}
+window.onload = function() { window.requestAnimationFrame(draw); }
 
 
-/********************************/
+
+
 
 
 function mutationFunction(phenotype) {
@@ -71,24 +74,14 @@ function mutationFunction(phenotype) {
 }
 
 function crossoverFunction(a, b) {
-    function cloneJSON( item ) {
-        return JSON.parse ( JSON.stringify ( item ) )
-    }
-
-    var x = cloneJSON(a), y = cloneJSON(b), cross = false;
-
-    for (var i in a.pos) {
-        if ( Math.random() * a.pos.length <= 1 ) { cross = !cross }
-        if (cross) {
-            x.pos[i] = b.pos[i];
-            y.pos[i] = a.pos[i];
-        }
-    }
-    return [ x , y ];
+    let x = cloneJSON(a)
+    let y = cloneJSON(b)
+    x.pos[0] = b.pos[0];
+    y.pos[1] = a.pos[1];
+    return Math.random() < .5 ? [x,y] : [y,x];
 }
 
-Math.square = function(x) { return Math.pow(x,2) }
-// this makes a 5x5 grid of hotspots with [0.5,0.5] being the lowest and [8.5,8.5] being the hottest
+
 function fitnessFunction(phenotype) {
     return positionScore(phenotype.pos[0],phenotype.pos[1])
 }
@@ -96,18 +89,17 @@ function fitnessFunction(phenotype) {
 function doesABeatBFunction(a,b) {
     let aScore = fitnessFunction(a)
     let bScore = fitnessFunction(b)
+    let distance = Math.sqrt(Math.sqr(a.pos[0] - b.pos[0]) + Math.sqr(a.pos[1] - b.pos[1]))
 
-    if ((aScore >= 0)&&(bScore < 0)) return true
+    if ((aScore >= 0)&&(bScore < 0)) return aScore
+    if ((aScore < 0)&&(bScore < 0)) return 1
+    if (distance > 2 && Math.random() > .1/distance ) return 0
 
-    let distance = Math.sqrt(Math.square(a.pos[0] - b.pos[0]) + Math.square(a.pos[1] - b.pos[1]))
-
-    if (distance > 2 && Math.random() > .1/distance ) return false
-
-    return aScore > bScore
+    return aScore - bScore
 }
 
 function createEmptyPhenotype() {
-    return { pos : [ 0.1 , 0.1 ] }
+    return { pos : [ .3 , .5 ] }
 }
 
 
@@ -124,7 +116,7 @@ setInterval(function(){
             return Math.abs(p.pos[0]-8.5) < 1 && Math.abs(p.pos[1]-8.5) < 1 ? 1 : 0
         }).reduce(function(a,b){ return a+b }) + " of " + ga.population().length
     }
-},100)
+},50)
 
 
 let ga = geneticAlgorithmConstructor({
